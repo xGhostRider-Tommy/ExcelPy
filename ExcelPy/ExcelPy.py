c0c313f3c6671559e7487dfa6e3b5197ac2a5eaa
Tommy 👻 <tommasoquadrelli@gmail.com>
2024-05-01 18:03:12 +0200

    aggiunta add e remove, remove ha un bug da fixare

--- ExcelPy/ExcelPy.py
+++ ExcelPy/ExcelPy.py
@@ -1,3 +1,5 @@
+import glob
+from token import COMMA
 from EditCell import ParseValue, tk, ttk, CppAPI
 
 
@@ -8,10 +10,14 @@ global root
 global currentFormula
 global table
 global currentCell
+global addY
+global addX
 
+a = [CppAPI.ExcelCell(CppAPI.DoubleCell(1))]
+a[0] = "hdahuiasd"
+a.pop()
 
 def Reload():
-    print("Reloading")
     global cellsXNumber
     global cellsYNumber
     global displayTable
@@ -27,15 +33,13 @@ def Reload():
                 )
             )
 
-def ChangeValue():
+def UpdateValue():
     global currentCell
     global table
     global currentFormula
 
-    print("ChangeValue")
     print(currentFormula.get())
     if (currentCell == None):
-        print(currentCell)
         return
     else:
         currentCell.set(ParseValue(table, currentFormula.get()))
@@ -45,13 +49,100 @@ def CellPressed(x, y):
     global currentCell
     global table
 
-    print("CellPressed")
     currentCell = table[y][x]
-    print(currentCell)
     return table[y][x].ptr().getString()
 
+def UpdateTable():
+    global cellsYNumber
+    global cellsXNumber
+    global table
+    global displayTable
 
+    for y in range(0, cellsYNumber):
+        if (len(displayTable) <= y):
+            displayTable.append([])
+        
+        for x in range(0, cellsXNumber):
+            if (len(displayTable[y]) <= x):
+                button = tk.Button(root, text="", height=1, width=10)
+                button.place(x = x*80, y = (y+2)*25)
+        
+                displayTable[y].append(button)
+            displayTable[y][x].config(command=None)
+                
+        while (len(displayTable[y]) != cellsXNumber):
+            displayTable[y][-1].destroy()
+            displayTable[y].pop()
             
+    while (len(displayTable) != cellsYNumber):
+        for element in displayTable[-1]:
+            element.destroy()
+        
+        displayTable.pop()
+        
+
+    for y in range(0, cellsYNumber):
+        if (len(table) <= y):
+            table.append([]);
+        
+        for x in range(0, cellsXNumber):
+            if (len(table[y]) <= x):
+                table[y].append(CppAPI.ExcelCell(CppAPI.DoubleCell(1)))
+        
+        while (len(table[y]) != cellsXNumber):
+            table[y].pop()
+    
+    print("cellsYNumber: " + str(cellsYNumber) + "\n")
+    print("len table: " + str(len(table)))
+    print("y: " + str(y))
+    while (len(table) != cellsYNumber):
+        for i in range(0, len(table[-1])):
+            table[-1].pop()
+        table.pop()
+        print("len table: " + str(len(table)))
+        print("y: " + str(y))
+    print("lets go")
+        
+
+
+    
+        
+
+
+    addX.place(x = cellsXNumber*65, y = 25)
+    addY.place(x = 0, y = (cellsYNumber+2)*25)
+    
+    removeX.place(x = (cellsXNumber)*65, y = 25)
+    removeY.place(x = 40, y = (cellsYNumber+2)*25)
+    
+    print("\n\n\n\n\n\n\n\n")
+    Reload()
+
+def AddX():
+    global cellsXNumber
+    cellsXNumber += 1
+    
+    UpdateTable()
+    
+def AddY():
+    global cellsYNumber
+    cellsYNumber += 1
+    
+    UpdateTable()
+
+    
+def RemoveX():
+    global cellsXNumber
+    cellsXNumber -= 1
+    
+    UpdateTable()
+    
+def RemoveY():
+    global cellsYNumber
+    cellsYNumber -= 1
+    
+    UpdateTable()
+
 
 cellsXNumber = 3
 cellsYNumber = 3
@@ -59,37 +150,21 @@ displayTable = []
 table = []
 currentCell = None
 root = tk.Tk()
+
 currentFormula = tk.StringVar()
 formulaBar = tk.Entry(root, textvariable=currentFormula)
-
 formulaBar.grid(row=0, column=0)
 
+updateButton = tk.Button(root, text="Update Value", command=UpdateValue)
+updateButton.place(x = 150, y = 0)
 
-idk = tk.Button(root, text="ChangeValue", command=ChangeValue)
-idk.place(x=100, y = 10)
+addX = tk.Button(root, text="+", command=AddX, height=1, width=4)
+addY = tk.Button(root, text="+", command=AddY, height=1, width=4)
 
+removeX = tk.Button(root, text="-", command=RemoveX, height=1, width=4)
+removeY = tk.Button(root, text="-", command=RemoveY, height=1, width=4)
 
+UpdateTable()
 
-
-for y in range(0, cellsYNumber):
-    table.append([]);
-    for x in range(0, cellsXNumber):
-        table[y].append(CppAPI.ExcelCell(CppAPI.DoubleCell(1)))
-
-
-for y in range(0, cellsYNumber):
-    displayTable.append([])
-    for x in range(0, cellsXNumber):
-        button = tk.Button(root, text="", height=1, width=8)
-        button.place(x=x*65,y=y*25+25)
-        
-        displayTable[y].append(button)
-
-Reload()
-
-
-root.state("zoomed") 
-
-
-
+root.state("zoomed")
 root.mainloop()
